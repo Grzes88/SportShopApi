6be5aa4d3ad458ba5aee556bc6542ad3375c9bee
Grzegorz Bączek <Gregor333000@gmail.com>
2024-04-23 10:39:44 +0200

    Create product in project

---
+++ src/SportShop.Application/Commands/Products/Handlers/CreateProductHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using SportShop.Application.Abstractions;
+using SportShop.Core.Entities;
+
+namespace SportShop.Application.Commands.Products.Handlers;
+
+public record CreateProductCommand(string Name) : IRequest;
+
+public sealed class CreateProductHandler : IRequestHandler<CreateProductCommand>
+{
+    private readonly IProductRepository _productRepository;
+
+    public CreateProductHandler(IProductRepository productRepository)
+        => _productRepository = productRepository;
+
+    public async Task Handle(CreateProductCommand request, CancellationToken token)
+    {
+        var product = Product.Create(request.Name);
+        await _productRepository.AddProductAsync(product, token);
+    }
+}
---
+++ src/SportShop.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using SportShop.Application.Abstractions;
+using SportShop.Core.Entities;
+using SportShop.Infrastructure.DAL;
+
+namespace SportShop.Infrastructure.Repositories;
+
+internal sealed class ProductRepository : IProductRepository
+{
+    private readonly SportShopDbContext _dbContext;
+
+    public ProductRepository(SportShopDbContext dbContext) 
+        => _dbContext = dbContext;
+
+    public async Task AddProductAsync(Product product, CancellationToken token)
+    {
+        await _dbContext.Products.AddAsync(product, token);
+        await _dbContext.SaveChangesAsync(token);
+    }
+}
